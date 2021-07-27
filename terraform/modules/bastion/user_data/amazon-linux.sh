95ade530fc7f99dfdbf855ef4eb51b90ba3810a0
Brodey Newman <brodeynewman@gmail.com>
2021-07-27 12:31:27 -0400

    chore: aws + kafka + java userdata install

--- terraform/modules/bastion/user_data/amazon-linux.sh
+++ terraform/modules/bastion/user_data/amazon-linux.sh
@@ -25,4 +25,16 @@ else
     systemctl status amazon-ssm-agent
 fi
 
+## Set up kafka-related stuff
+sudo yum -y install java-1.8.0
+sudo yum -y install wget
+sudo yum -y install unzip
+wget https://archive.apache.org/dist/kafka/2.2.1/kafka_2.12-2.2.1.tgz
+tar -xzf kafka_2.12-2.2.1.tgz
+
+## set up AWS cli
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+
 ${user_data}
